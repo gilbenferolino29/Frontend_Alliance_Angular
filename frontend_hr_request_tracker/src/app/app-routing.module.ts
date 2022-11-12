8c75bc30276f8dc6d56786df9c042751aa127951
Gilben Ferolino <gilbenferolinoz@gmail.com>
2022-11-12 19:22:26 +0800

    Partial routing added

--- frontend_hr_request_tracker/src/app/app-routing.module.ts
+++ frontend_hr_request_tracker/src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminHomeComponent } from './screens/admin-screens/home-ticket-admin/admin-home/admin-home.component';
+import { LandingpageComponent } from './screens/landing-page/landingpage/landingpage.component';
+import { UserHomeComponent } from './screens/user-screens/home-ticket-user/user-home/user-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: LandingpageComponent,
+    
+
+  },
+  {
+    path:'home',
+    component: UserHomeComponent, 
+  },
+  {
+    path:'admin',
+    component: AdminHomeComponent,
+
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+ }
