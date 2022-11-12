88d9becf19d5f3177a074347f6ba8ebeb04c0081
Gilben Ferolino <gilbenferolinoz@gmail.com>
2022-11-12 15:07:27 +0800

    Added different components-screens and Http Service Created
    Admin 
    User
    Landingpage // To be determined if landing page will be same with login page
    Login

--- frontend_hr_request_tracker/src/app/app.module.ts
+++ frontend_hr_request_tracker/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingpageComponent } from './screens/landing-page/landingpage/landingpage.component';
+import { LoginComponent } from './screens/login/login/login.component';
+import { RegisterComponent } from './screens/register/register/register.component';
+import { UserHomeComponent } from './screens/user-screens/home-ticket-user/user-home/user-home.component';
+import { AdminHomeComponent } from './screens/admin-screens/home-ticket-admin/admin-home/admin-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingpageComponent,
+    LoginComponent,
+    RegisterComponent,
+    UserHomeComponent,
+    AdminHomeComponent
   ],
   imports: [
     BrowserModule,
