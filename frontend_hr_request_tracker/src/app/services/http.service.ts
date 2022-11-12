b8d1ddd3525ffb08983340d9cf91569134d473b5
Gilben Ferolino <gilbenferolinoz@gmail.com>
2022-11-12 15:07:27 +0800

    Added different components-screens
    Admin 
    User
    Landingpage // To be determined if landing page will be same with login page
    Login

---
+++ frontend_hr_request_tracker/src/app/services/http.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  constructor(private http:HttpClient) { }
+
+
+
+  //HTTP GET
+  public get (url:string){
+    return this.http.get(url);
+  }
+  //HTTP POST
+  public post (url: string, data:any){
+    return this.http.post(url,data);
+  }
+  //HTTP DELETE
+  public delete(url:string){
+    return this.http.delete(url);
+  }
+  //HTTP PATCH
+  public patch (url:string, data:any){
+    return this.http.patch(url,data);
+  }
+}
