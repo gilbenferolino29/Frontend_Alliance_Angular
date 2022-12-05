9be9ffa0301276b401ef3555140e00be0d675c64
Kian <shanekiantiu@gmail.com>
2022-12-06 07:57:59 +0800

    Fix createRole form validation

--- frontend_hr_request_tracker/src/app/screens/roles/create-role/create-role.component.ts
+++ frontend_hr_request_tracker/src/app/screens/roles/create-role/create-role.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { QueryService } from 'src/app/services/query.service';
 
@@ -8,25 +9,44 @@ import { QueryService } from 'src/app/services/query.service';
   styleUrls: ['./create-role.component.scss']
 })
 export class CreateRoleComponent implements OnInit {
-  roleName: any;
-  roleAbbv: any;
+  form = this.fb.group({
+    roleName: ['', [Validators.required]],
+    roleAbbv: ['', [Validators.required]]
+  });
   
   constructor(
     private queryService: QueryService,
-    public dialogRef: MatDialogRef<CreateRoleComponent>
+    public dialogRef: MatDialogRef<CreateRoleComponent>,
+    private fb: FormBuilder
   ) { }
 
   ngOnInit(): void {
   }
 
   createRole() {
-    let formData: FormData = new FormData();
+    if(this.form.valid) {
+      let formData: FormData = new FormData();
 
-    formData.append('roleName', this.roleName.toString());
-    formData.append('roleAbbv', this.roleAbbv.toString());
+      formData.append('roleName', this.roleName.value!.toString());
+      formData.append('roleAbbv', this.roleAbbv.value!.toString());
+  
+      this.queryService.createRole(formData).subscribe(res => {
+        this.dialogRef.close(res);
+      });
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
 
-    this.queryService.createRole(formData).subscribe(res => {
-      this.dialogRef.close(res);
-    });
+  get f() {
+    return this.form.controls;
+  }
+
+  get roleName() {
+    return this.form.controls.roleName;
+  }
+
+  get roleAbbv() {
+    return this.form.controls.roleAbbv;
   }
 }
