e971b7346ff603880177fa1bddaf70956d6ccd28
Kian <shanekiantiu@gmail.com>
2022-12-06 08:02:14 +0800

    Fix updateRole form validation

--- frontend_hr_request_tracker/src/app/screens/roles/update-role/update-role.component.ts
+++ frontend_hr_request_tracker/src/app/screens/roles/update-role/update-role.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Role } from 'src/app/models/Role';
 import { QueryService } from 'src/app/services/query.service';
@@ -9,26 +10,51 @@ import { QueryService } from 'src/app/services/query.service';
   styleUrls: ['./update-role.component.scss']
 })
 export class UpdateRoleComponent implements OnInit {
+  form = this.fb.group({
+    roleID: [this.data['role'].roleID],
+    roleName: [this.data['role'].roleName, [Validators.required]],
+    roleAbbv: [this.data['role'].roleAbbv, [Validators.required]]
+  });
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     private queryService: QueryService,
-    public dialogRef: MatDialogRef<UpdateRoleComponent>
+    public dialogRef: MatDialogRef<UpdateRoleComponent>,
+    private fb: FormBuilder
     ) { }
 
   ngOnInit(): void {
   }
 
-  updateRole(role: Role) {
-    let formData: FormData = new FormData();
+  updateRole() {
+    if(this.form.valid) {
+      let formData: FormData = new FormData();
 
-    formData.append('roleID', role.roleID.toString());
-    formData.append('roleName', role.roleName.toString());
-    formData.append('roleAbbv', role.roleAbbv.toString());
-
-    this.queryService.updateRole(formData).subscribe(res => {
-      this.dialogRef.close(res);
-    });
+      formData.append('roleID', this.roleID.value!.toString());
+      formData.append('roleName', this.roleName.value!.toString());
+      formData.append('roleAbbv', this.roleAbbv.value!.toString());
+  
+      this.queryService.updateRole(formData).subscribe(res => {
+        this.dialogRef.close(res);
+      });
+    } else {
+      this.form.markAllAsTouched();
+    }
   }
 
+  get f() {
+    return this.form.controls;
+  }
+
+  get roleID() {
+    return this.form.controls.roleID;
+  }
+
+  get roleName() {
+    return this.form.controls.roleName;
+  }
+
+  get roleAbbv() {
+    return this.form.controls.roleAbbv;
+  }
 }
