9b404690ab71faa4bab1ffe1f3159b6ddb4e9ab6
Kian <shanekiantiu@gmail.com>
2022-12-06 08:23:45 +0800

    Fix updateUser form validation

--- frontend_hr_request_tracker/src/app/screens/users/update-user/update-user.component.ts
+++ frontend_hr_request_tracker/src/app/screens/users/update-user/update-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { firstValueFrom } from 'rxjs';
 import { User } from 'src/app/models/IUser';
@@ -13,10 +14,21 @@ import { QueryService } from 'src/app/services/query.service';
 export class UpdateUserComponent implements OnInit {
   roleList: any = [];
 
+  form = this.fb.group({
+    userID: [this.data['user'].userID, [Validators.required]],
+    userRole: [this.data['user'].userRole.roleID, [Validators.required]],
+    username: [this.data['user'].username, [Validators.required]],
+    userFName: [this.data['user'].userFName, [Validators.required]],
+    userLName: [this.data['user'].userLName, [Validators.required]],
+    userPassword: [{value: this.data['user'].userPassword, disabled: true}, [Validators.required]],
+    userEmail: [this.data['user'].userEmail, [Validators.required, Validators.email]]
+  });
+
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     private queryService: QueryService,
-    public dialogRef: MatDialogRef<UpdateUserComponent>
+    public dialogRef: MatDialogRef<UpdateUserComponent>,
+    private fb: FormBuilder
   ) { }
 
   async ngOnInit(): Promise<void> {
@@ -27,28 +39,56 @@ export class UpdateUserComponent implements OnInit {
     this.roleList = await firstValueFrom(this.queryService.getAllRoles()) as Role[];
   }
 
-  updateUser(user: User) {
-    let formData: FormData = new FormData();
+  updateUser() {
+    if(this.form.valid) {
+      let formData: FormData = new FormData();
 
-    console.log(user);
-
-    formData.append('userID', user.userID.toString());
-    formData.append('userRole', user.userRole.roleID.toString());
-    formData.append('username', user.username.toString());
-    formData.append('userPassword', user.userPassword.toString());
-    formData.append('userFName', user.userFName.toString());
-    formData.append('userLName', user.userLName.toString());
-    formData.append('userEmail', user.userEmail.toString());
-
-    this.queryService.updateUser(formData).subscribe(res => {
-      this.dialogRef.close(res);
-    });
+      formData.append('userID', this.userID.value!.toString());
+      formData.append('userRole', this.userRole.value!.toString());
+      formData.append('username', this.username.value!.toString());
+      formData.append('userPassword', this.userPassword.value!.toString());
+      formData.append('userFName', this.userFName.value!.toString());
+      formData.append('userLName', this.userLName.value!.toString());
+      formData.append('userEmail', this.userEmail.value!.toString());
+  
+      this.queryService.updateUser(formData).subscribe(res => {
+        this.dialogRef.close(res);
+      });
+    } else {
+      this.form.markAllAsTouched();
+    }
   }
 
-  selectRole(role: any, event: any) {
-    if(event.isUserInput) {
-      this.data['user'].role = role;
-    }
+  get f() {
+    return this.form.controls;
+  }
+
+  get userID() {
+    return this.form.controls.userID;
+  }
+
+  get username() {
+    return this.form.controls.username;
+  }
+
+  get userFName() {
+    return this.form.controls.userFName;
+  }
+
+  get userLName() {
+    return this.form.controls.userLName;
+  }
+
+  get userPassword() {
+    return this.form.controls.userPassword;
+  }
+
+  get userEmail() {
+    return this.form.controls.userEmail;
+  }
+
+  get userRole() {
+    return this.form.controls.userRole;
   }
 
 }
